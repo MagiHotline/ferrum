785bf86178608d07ea53fe1eb362f8a781628ef9
Magi <paoloimbrmagi@gmail.com>
2026-04-15 09:21:53 +0200

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ferrum
+
+*Ferrum* is a WIP Video Rendered built on Metal  (a low-level, low-overhead hardware-accelerated 3D graphic and compute shader API created by Apple) using Rust and [objc2-metal](https://crates.io/crates/objc2-metal).
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use objc2_metal::{MTLCreateSystemDefaultDevice, MTLDevice};
+
+fn main() {
+    let device =
+        MTLCreateSystemDefaultDevice()
+        .expect("Failed to find a Metal device. Are you on a Mac?");
+
+    println!("Successfully initialized Metal");
+    println!("Device: {}", device.name());
+}

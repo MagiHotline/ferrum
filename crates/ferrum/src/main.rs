bf360db82874dd677d9673dd871c1bdb4600464e
Magi <paoloimbrmagi@gmail.com>
2026-04-22 10:25:25 +0200

    refactored from glfw_sys C ffi to full glfw crate

--- crates/ferrum/src/main.rs
+++ crates/ferrum/src/main.rs
@@ -4,10 +4,6 @@ mod mtleng;
 use mtleng::{MTLEngine};
 
 fn main() {
-
-    let engine = unsafe { MTLEngine::new() };
-    unsafe {
-        engine.run();
-        engine.cleanup();
-    };
+    let mut engine = MTLEngine::new();
+    engine.run();
 }
--- crates/ferrum/src/mtleng.rs
+++ crates/ferrum/src/mtleng.rs
@@ -1,8 +1,4 @@
-// ; MTLEngine is where is stashed all the rendering engine logic
-use std::ptr::null_mut;
-use glfw::ffi::{GLFW_CLIENT_API, GLFW_NO_API, GLFWwindow,
-    glfwCreateWindow, glfwGetCocoaWindow, glfwInit,
-    glfwPollEvents, glfwTerminate, glfwWindowHint, glfwWindowShouldClose};
+use glfw::{GlfwReceiver, PWindow, WindowEvent, WindowMode};
 use objc2_app_kit::NSWindow;
 use objc2_metal::{MTLCreateSystemDefaultDevice, MTLDevice, MTLPixelFormat};
 use objc2::{rc::Retained, runtime::ProtocolObject};
@@ -11,34 +7,43 @@ use objc2_quartz_core::CAMetalLayer;
 // Only OBJ-C objects need Retained
 
 pub struct MTLEngine {
+    glfw: glfw::Glfw,
+    events: GlfwReceiver<(f64, WindowEvent)>,
     device: Retained<ProtocolObject<dyn MTLDevice>>,
-    glfw_window: *mut GLFWwindow,
+    glfw_window: PWindow,
     metal_window: Retained<NSWindow>,
     metal_layer: Retained<CAMetalLayer>
 }
 
 impl MTLEngine {
-    pub unsafe fn new() -> Self {
-        glfwInit();
+
+    pub fn new() -> Self {
+        let mut glfw = glfw::init_no_callbacks().expect("Failed to init glfw");
         // Tell GLFW to not create OPENGL graphics context
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi);
+        //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         // HARDCODED WIDHT AND LENGTH FOR NOW
-        let glfw_window = glfwCreateWindow(
-            800,
-            600,
-            c"Metal Window".as_ptr(),
-            null_mut(),
-            null_mut()
-        );
+        /*let glfw_window = */
+        let (mut glfw_window, events) = glfw.create_window(
+            800, 600,
+            "Metal Window",
+            WindowMode::Windowed
+        ).expect("Failed to create window.");
 
         let device =
             MTLCreateSystemDefaultDevice()
             .expect("Failed to find a Metal device.");
 
-        let metal_window =
-            Retained::retain(glfwGetCocoaWindow(glfw_window) as *mut NSWindow)
-                .expect("Unable to find Objc Handle");
+        // let metal_window =
+        //   Retained::retain(glfwGetCocoaWindow(glfw_window) as *mut NSWindow)
+        //       .expect("Unable to find Objc Handle");
+        //
+        let metal_window = unsafe {
+                Retained::retain(glfw::Window::get_cocoa_window(&glfw_window) as *mut NSWindow)
+                    .expect("Unable to find Objc handle")
+        };
 
+        // Link device and layer to the window
         let metal_layer = CAMetalLayer::new();
         metal_layer.setDevice(Some(&device));
         metal_layer.setPixelFormat(MTLPixelFormat::BGRA8Unorm);
@@ -47,6 +52,8 @@ impl MTLEngine {
 
         Self
         {
+            glfw,
+            events,
             device,
             glfw_window,
             metal_window,
@@ -54,14 +61,10 @@ impl MTLEngine {
         }
     }
 
-    pub unsafe fn run(&self) {
-        while glfwWindowShouldClose(self.glfw_window) == 0 {
-            glfwPollEvents();
+    pub fn run(&mut self) {
+        while !glfw::Window::should_close(&self.glfw_window) {
+            self.glfw.poll_events();
         }
-    }
-
-    pub unsafe fn cleanup(&self) {
-        glfwTerminate();
     }
 
 }
